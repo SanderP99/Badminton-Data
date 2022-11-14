e701309c817f7512f68c701f5f1aa867f416968e
Sander Prenen <sander.prenen@devoteam.com>
2022-11-14 14:21:40 +0100

    Scrape tournaments every monday

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from src.scraping.players import scrape_players
+from src.scraping.tournaments import scrape_tournaments
 
 if __name__ == "__main__":
     scrape_players.scrape_players(2000)
+    scrape_tournaments.scrape_tournaments()
